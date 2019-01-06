8aa97923f890641ec604b0c9baeb6e92f30bcb87
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-06 15:57:16 +0800

    print event message

--- app_router.js
+++ app_router.js
@@ -14,6 +14,7 @@ const linebotParser = bot.parser();
 app.post('/linewebhook', linebotParser);
 
 bot.on('message', function (event) {
+    console.log( event )
     event.reply(event.message.text).then(function (data) {
         console.log('Success', data);
     }).catch(function (error) {
