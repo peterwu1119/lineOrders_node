24582c3e4d072ce410fb9ae271aec8f3378276ca
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-12 18:44:02 +0800

    reformat code

--- app_router.js
+++ app_router.js
@@ -24,47 +24,14 @@ bot.on('join' , function(event){
     bot.push( event.source.userId , 'welcome');
 })
 
+// reply token can only be used one time , 
+// if we want to reply multiple times in single webhook event
+// use push message
+
 bot.on('message', function (event) {
-    console.log( event )
-    //bot.push( event.source.userId , 'http://www.google.com');
-
     if( event.message.text === "我要團購" ){
-        create_flex_message( event.source.userId );
+        create_flex_group_buy_message( event.source.userId );
     }
-
-    /*
-    event.reply({
-        type: 'sticker',
-        packageId: '1',
-        stickerId: '1'
-    })
-    */
-
-    console.log( event.source.profile() )
-    event.source.profile().then(function (profile) {
-        console.log( profile )
-        //bot.push( event.source.userId , 'Hello ' + profile.displayName );
-
-        // reply token can only be used one time , 
-        // if we want to reply multiple times in single webhook event
-        // use push message 
-        /*
-        event.reply('Hello ' + profile.displayName).then(function(data){
-            console.log('Success' , data);
-        }).catch(function(error){
-            console.log('Error' , error);
-        });
-        */
-    });
-
-
-    /*
-    event.reply(event.message.text).then(function (data) {
-        console.log('Success', data);
-    }).catch(function (error) {
-        console.log('Error', error);
-    });
-    */
 });
 
 //Express API --------- App.get('path', callback function);
@@ -77,93 +44,57 @@ app.post('/ajax', function (request, response) {
     response.send("response by ajax");
 });
 
-function create_flex_message( user_id ){
-    console.log( user_id );
-    /*
-    request.post(
-        'https://api.line.me/v2/bot/message/push',
-        { json: { key: 'value' } },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body)
+function create_flex_group_buy_message( user_id ){
+    
+    var message =  {
+        "type": "flex",
+        "altText": "This is a Flex Message",
+        "contents": {
+            "type": "bubble",
+            "hero": {
+                "type": "image",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover",
+                "action": {
+                "type": "uri",
+                "uri": "http://linecorp.com/"
+                }
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                    "type": "uri",
+                    "label": "建立團購",
+                    "uri": process.env.DOMAIN_NAME
+                    }
+                },
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                    "type": "uri",
+                    "label": "團購列表",
+                    "uri": process.env.DOMAIN_NAME
+                    }
+                },
+                {
+                    "type": "spacer",
+                    "size": "sm"
+                }
+                ],
+                "flex": 0
             }
         }
-    );
-    */
-
-    const options = {
-        url: 'https://api.line.me/v2/bot/message/push',
-        method : 'POST',
-        json : true,
-        body : {
-            'to' : user_id, 
-            'messages' : [
-                {
-                    "type": "flex",
-                    "altText": "This is a Flex Message",
-                    "contents": {
-                        "type": "bubble",
-                        "hero": {
-                          "type": "image",
-                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                          "size": "full",
-                          "aspectRatio": "20:13",
-                          "aspectMode": "cover",
-                          "action": {
-                            "type": "uri",
-                            "uri": "http://linecorp.com/"
-                          }
-                        },
-                        "footer": {
-                          "type": "box",
-                          "layout": "vertical",
-                          "spacing": "sm",
-                          "contents": [
-                            {
-                              "type": "button",
-                              "style": "link",
-                              "height": "sm",
-                              "action": {
-                                "type": "uri",
-                                "label": "建立團購",
-                                "uri": process.env.DOMAIN_NAME
-                              }
-                            },
-                            {
-                              "type": "button",
-                              "style": "link",
-                              "height": "sm",
-                              "action": {
-                                "type": "uri",
-                                "label": "團購列表",
-                                "uri": process.env.DOMAIN_NAME
-                              }
-                            },
-                            {
-                              "type": "spacer",
-                              "size": "sm"
-                            }
-                          ],
-                          "flex": 0
-                        }
-                      }
-                }
-            ]
-        },
-        headers: {
-            'Authorization': 'Bearer {' + process.env.CHANNEL_TOKEN +'}'
-        }
-    };
-
-    function callback(error, response, body) {
-        console.log( error );
-        console.log( body );
-        console.log( response.statusCode );
     }
-
-    console.log('before request');
-    console.log( options )
-    console.log( process.env);
-
-    request( options, callback );
+    bot.push( user_id , message );
 }
