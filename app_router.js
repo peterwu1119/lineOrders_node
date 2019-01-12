efe71d81ecace472b27aa29eb188c21831882a90
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-12 22:23:30 +0800

    change host_url

--- app_router.js
+++ app_router.js
@@ -17,7 +17,7 @@ var host_url = '';
 
 app.post('/linewebhook',
         function(req ,res , next){ 
-            host_url = req.protocol + '://' + req.get('host') + req.originalUrl;
+            host_url = req.protocol + '://' + req.get('host');
             next();
         } , 
         linebotParser
