075b7645b027c0c1e47febe3be41ba79a25edd47
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-13 21:49:29 +0800

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,42 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users/1">Users 1</Link></li>
+          <li><Link to="/users/2">Users 2</Link></li>
+        </ul>
+        {this.props.children} 
+      </div>
+    );
+  }
+}
+
+class About extends React.Component {
+  render () {
+    return (<h1>About</h1>);
+  }
+}
+
+class Users extends React.Component {
+  render () {
+    const id = this.props.params.userId;
+    return (
+      <div>
+        user id: {id}
+      </div>
+    )
+  }
+}
+
+/*
 class App extends Component {
 
 	constructor (props) {
@@ -33,5 +68,6 @@ class App extends Component {
 		);
 	}
 }
+*/
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, hashHistory } from 'react-router'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  (
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <Route path="about" component={About}/>
+        <Route path="users/:userId" component={Users} />
+      </Route>
+    </Router>
+  ),
+  document.getElementById('root')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
