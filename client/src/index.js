764a609ce22af371d14eb405908eb67355fed961
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-14 22:39:06 +0800

    change router import

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import './index.css';
 import App from './App';
