07b8b27728a77a2cbcf85c2783c225016e9568cf
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-16 17:30:36 +0800

    test

--- app_router.js
+++ app_router.js
@@ -4,6 +4,9 @@ const request = require('request');
 var bodyParser = require('body-parser')
 var app = module.exports = express();
 
+app.use( bodyParser.urlencoded( { extended : false }) );
+app.use( bodyParser.json() );
+
 const bot = linebot({
     channelId: process.env.CHANNEL_ID,
     channelSecret: process.env.CHANNEL_SECRET,
