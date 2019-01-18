a791106291eff39029c3636aa7f32a6fa8bd6e45
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-18 17:45:58 +0800

    add blob

--- client/src/components/CreateGroupBuy.js
+++ client/src/components/CreateGroupBuy.js
@@ -46,7 +46,7 @@ class CreateGroupBuy extends React.Component {
     */
 
     var formData = new FormData();
-    formData.append( 'image', document.getElementById('groupBuyImage').files[0] );
+    formData.append( 'image', new Blob([ document.getElementById('groupBuyImage').files[0] ]) );
     axios.post('/api/uploadImageToImgur', formData, {
         
     })
