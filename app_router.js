7027e17db2d70d647c4e3663207251b369a1980d
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-20 17:23:09 +0800

    test

--- app_router.js
+++ app_router.js
@@ -78,7 +78,7 @@ app.post('/api/uploadImageToImgur', function(request, response){
         fetch('https://api.imgur.com/3/image', {
             method: 'POST',
             headers: {
-                'Authorization' : 'Client-ID ' + process.env.IMGUR_CLIENT_ID
+                'Authorization' : 'Client-ID abc' 
             },
             body: JSON.stringify( form )
         })
