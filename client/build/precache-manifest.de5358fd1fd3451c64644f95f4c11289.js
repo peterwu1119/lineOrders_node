4d46f40b394153f0b886ff30de7882a7583ab505
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-23 07:40:35 +0800

    local build react code and push to heroku , not directly build on heroku

---
+++ client/build/precache-manifest.de5358fd1fd3451c64644f95f4c11289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e8f4549de0fc9146cb6",
+    "url": "/static/css/main.450bf04d.chunk.css"
+  },
+  {
+    "revision": "3e8f4549de0fc9146cb6",
+    "url": "/static/js/main.3e8f4549.chunk.js"
+  },
+  {
+    "revision": "a6295c190217d16d52d8",
+    "url": "/static/js/1.a6295c19.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "05e89bacd07d10f6ef7b6779de8364dd",
+    "url": "/index.html"
+  }
+];
