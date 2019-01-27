f9fe03a6cc4d19f3d211d9e8d811c605bcc738aa
peter.wu <peter@peterdeMacBook-Pro.local>
2019-01-27 12:54:51 +0800

    build react code

---
+++ client/build/precache-manifest.d123888b74f34041194e0aec373a41b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "611ea7bb3ff916fbb9e6",
+    "url": "/static/css/main.450bf04d.chunk.css"
+  },
+  {
+    "revision": "611ea7bb3ff916fbb9e6",
+    "url": "/static/js/main.611ea7bb.chunk.js"
+  },
+  {
+    "revision": "f437c5545da5af4b51ed",
+    "url": "/static/js/1.f437c554.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4fe9606c40970872efe6f63f6709cc7d",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e6a0d6f5d2b7c88a5e276bac0ec1a3d.js"
+  "/precache-manifest.d123888b74f34041194e0aec373a41b0.js"
 );
 
 workbox.clientsClaim();
